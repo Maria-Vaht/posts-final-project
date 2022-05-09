2a520e11427e06c2d6ef2b6b3b8ed6c1ba7b231e
Maria-Vaht <96318594+Maria-Vaht@users.noreply.github.com>
2022-05-09 14:54:03 +0300

    Added hooks, updated Post and PostPage

--- src/components/FormDialog/index.js
+++ src/components/FormDialog/index.js
@@ -2,65 +2,68 @@ import React, { useState, useEffect, useContext } from 'react'
 import GlobalContext from '../../contexts/globalContext'
 import { Dialog, Button, TextField, DialogActions, DialogTitle, DialogContent } from '@mui/material'
 import api from '../../utils/api'
-import { useNavigate, useParams } from 'react-router-dom';
 
-export const EditPostDialog = () => {
-    const { editPostDialogState, setEditPostDialogState, setPostList } = useContext(GlobalContext)
+export const FormDialog = () => {
+    const { formDialogState: { postId, isOpen }, setFormDialogState, setPostList } = useContext(GlobalContext)
 
-    const params = useParams()
-    const navigate = useNavigate()
-
-    const [title, setTitle] = useState('');
-    const [text, setText] = useState('');
-    const [image, setImage] = useState('');
-    const [tags, setTags] = useState('');
+    const [title, setTitle] = useState('')
+    const [text, setText] = useState('')
+    const [image, setImage] = useState('')
+    const [tags, setTags] = useState('')
 
     useEffect(() => {
-        api.getPosts(params.PostId)
-            .then(({ title, text, image, tags }) => {
-                setTitle(title)
-                setText(text)
-                setImage(image)
-                setTags(tags.join(', '))
-            })
-            .catch(err => alert(err));
-    }, []);
+        if (postId) {
+            api.getPosts(postId)
+                .then((post) => {
+                    setTitle(post?.title)
+                    setText(post?.text)
+                    setImage(post?.image)
+                    setTags(post?.tags.join(', '))
+                })
+                .catch(err => alert(err))
+        }
+    }, [postId]);
 
     const handleClose = () => {
-        setEditPostDialogState(() => {
+        setFormDialogState(() => {
             return {
                 isOpen: false,
-            };
+                postId: null,
+            }
         })
-        navigate('/')
     }
 
     const handleSubmit = () => {
-        api.editPost(params.PostId, title, text, image, tags)
-            .then(() => handleClose())
-            .catch(err => alert(err))
+        if (postId) {
+            api.editPost(postId, title, text, image, tags)
+                .then(() => handleClose())
+                .catch(err => alert(err))
+        } else {
+            api.createPost(title, text, image, tags)
+                .then((newPost) => setPostList(prevState => [newPost, ...prevState]))
+                .then(() => handleClose())
+                .catch(err => alert(err))
+        }
     };
 
     return (
         <div>
-            <Dialog disablePortal={true} open={editPostDialogState.isOpen} onClose={handleClose}>
-                <DialogTitle>Edit post</DialogTitle>
+            <Dialog disablePortal={true} open={isOpen} onClose={handleClose}>
+                <DialogTitle>{postId ? 'Edit post' : 'Create post'}</DialogTitle>
                 <DialogContent>
                     <TextField
                         autoFocus
                         margin="dense"
-                        // id="title"
                         label="Title"
                         fullWidth
                         variant="standard"
-                        value={title || ''}
+                        value={title}
                         onChange={({ target }) => {
                             setTitle(target.value);
                         }}
                     />
                     <TextField
                         margin="dense"
-                        // id="text"
                         label="Text"
                         fullWidth
                         variant="standard"
@@ -71,7 +74,6 @@ export const EditPostDialog = () => {
                     />
                     <TextField
                         margin="dense"
-                        // id="image"
                         label="Image"
                         fullWidth
                         variant="standard"
@@ -82,7 +84,6 @@ export const EditPostDialog = () => {
                     />
                     <TextField
                         margin="dense"
-                        // id="tags"
                         label="Tags"
                         fullWidth
                         variant="standard"
@@ -94,7 +95,7 @@ export const EditPostDialog = () => {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleSubmit}>Edit</Button>
+                    <Button onClick={handleSubmit}>{postId ? 'Edit' : 'Create'}</Button>
                 </DialogActions>
             </Dialog>
         </div>
